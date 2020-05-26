7b878af0b57234401fe723c8a522ec26132230b4
Louis Coleman <coleloui18@gmail.com>
2020-05-26 09:56:36 -0700

    merge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Router,
+  Route,
+} from "react-router-dom";
+import "./App.css";
+import Wrapper from "./components/Wrapper";
+import Main from "./pages/Main";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
-function App() {
+const App = () => {
+  document.title = "Louis Coleman Portfolio";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Wrapper>
+    </Router>
   );
-}
+};
 
 export default App;
