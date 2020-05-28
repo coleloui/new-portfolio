165b32f1f8e5aed934f7e44689dd206e2e294744
Louis Coleman <coleloui18@gmail.com>
2020-05-27 17:11:29 -0700

    contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import Scroll from "./Progress";
+import { Link, useState, useEffect } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -8,9 +7,9 @@ export default function Navbar() {
       id="top-of-page"
       className="grid bg-teal-400 rounded-md font-bold h-10 border-4 border-teal-600 sticky top-0"
     >
-      {/* <Scroll /> */}
       <div className="text-center flex justify-around self-center">
-        <Link to="/" className="hover:underline hover:text-gray-600">
+        Louis Coleman
+        {/* <Link to="/" className="hover:underline hover:text-gray-600">
           Home
         </Link>
         <Link to="/projects" className="hover:underline hover:text-gray-600">
@@ -18,7 +17,7 @@ export default function Navbar() {
         </Link>
         <Link to="/contact" className="hover:underline hover:text-gray-600">
           Contact Me
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="container rounded-md border-4 border-teal-600 shadow-xl bg-gray-400 col-start-2 mt-10">
+      <div>hi</div>
+    </div>
+  );
+}
